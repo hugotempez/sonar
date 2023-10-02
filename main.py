7a1fb38dca097a6b879bf63e793fd974b0e6191b
Hugo Tempez <hugotempezpro@gmail.com>
2023-10-02 12:05:53 +0200

    Resize image, affichage image

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import tkinter
+from PIL import ImageTk, Image
+from resizeimage import resizeimage
+
+
+window = tkinter.Tk()
+window.geometry("1200x600")
+window.resizable(False, False)
+frame = tkinter.Frame(window, width=1200, height=600)
+frame.pack()
+frame.place(anchor='center', relx=0.5, rely=0.5)
+img = Image.open("Map_sonar.png")
+out_image = resizeimage.resize_width(img, 1200)
+out_image.save("resize.png")
+img = ImageTk.PhotoImage(Image.open("resize.png"))
+label = tkinter.Label(frame, image=img)
+label.pack()
 
 
 def gui():
-    tkinter._test()
+    window.mainloop()
 
 
 def main():
     gui()
-    print("ok")
 
 
 main()
