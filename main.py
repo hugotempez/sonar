66895520a033c4f989d2d1bd053984c151d4a38d
Hugo Tempez <hugotempezpro@gmail.com>
2023-10-02 14:31:27 +0200

    canva ok, import image ok, creation cercle ok

--- main.py
+++ main.py
@@ -2,26 +2,38 @@ import tkinter
 from PIL import ImageTk, Image
 from resizeimage import resizeimage
 
+width = 1200
+height = 600
 
 window = tkinter.Tk()
 window.geometry("1200x600")
 window.resizable(False, False)
 
-frame_map = tkinter.Frame(window, width=1200, height=600, background="")
-frame_map.pack()
-frame_map.place(anchor='center', relx=0.5, rely=0.5)
-
+canva = tkinter.Canvas(width=width, height=height, bg="white")
 img = Image.open("Map_sonar.png")
-out_image = resizeimage.resize_width(img, 1200)
+out_image = resizeimage.resize_width(img, width)
 out_image.save("resize.png")
 
 img_map = ImageTk.PhotoImage(Image.open("resize.png"))
+canva.pack()
+canva.create_image(width/2, height/2, anchor="center", image=img_map)
 
-label_map = tkinter.Label(frame_map, image=img_map)
 
-label_map.pack()
+# frame_map = tkinter.Frame(window, width=1200, height=600)
+# frame_map.pack()
+# frame_map.place(anchor="center", relx=0.5, rely=0.5)
+#
+# img = Image.open("Map_sonar.png")
+# out_image = resizeimage.resize_width(img, 1200)
+# out_image.save("resize.png")
+#
+# img_map = ImageTk.PhotoImage(Image.open("resize.png"))
+#
+# label_map = tkinter.Label(frame_map, image=img_map)
+#
+# label_map.pack()
 
-robot = tkinter.Canvas().create_oval(40, 40, 40, 40, fill="black")
+robot = canva.create_oval(175, 100, 100, 175, width=3)
 
 
 def gui():
