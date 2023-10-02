84aa92317fe7715eb2ff66894c6bdc848408fe24
Hugo Tempez <hugotempezpro@gmail.com>
2023-10-02 12:37:57 +0200

    Resize image, affichage image

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ from resizeimage import resizeimage
 window = tkinter.Tk()
 window.geometry("1200x600")
 window.resizable(False, False)
-frame = tkinter.Frame(window, width=1200, height=600)
-frame.pack()
-frame.place(anchor='center', relx=0.5, rely=0.5)
+
+frame_map = tkinter.Frame(window, width=1200, height=600, background="")
+frame_map.pack()
+frame_map.place(anchor='center', relx=0.5, rely=0.5)
+
 img = Image.open("Map_sonar.png")
 out_image = resizeimage.resize_width(img, 1200)
 out_image.save("resize.png")
-img = ImageTk.PhotoImage(Image.open("resize.png"))
-label = tkinter.Label(frame, image=img)
-label.pack()
+
+img_map = ImageTk.PhotoImage(Image.open("resize.png"))
+
+label_map = tkinter.Label(frame_map, image=img_map)
+
+label_map.pack()
+
+robot = tkinter.Canvas().create_oval(40, 40, 40, 40, fill="black")
 
 
 def gui():
