583367b85d434836e0433264f593bc14ec76e6ab
Hugo Tempez <hugotempezpro@gmail.com>
2023-10-02 14:56:42 +0200

    Deplacement robot op

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tkinter
+import tkinter, time
 from PIL import ImageTk, Image
 from resizeimage import resizeimage
+
 
 width = 1200
 height = 600
@@ -18,30 +19,22 @@ img_map = ImageTk.PhotoImage(Image.open("resize.png"))
 canva.pack()
 canva.create_image(width/2, height/2, anchor="center", image=img_map)
 
+robot = canva.create_oval(10, 10, 40, 40, width=3)
 
-# frame_map = tkinter.Frame(window, width=1200, height=600)
-# frame_map.pack()
-# frame_map.place(anchor="center", relx=0.5, rely=0.5)
-#
-# img = Image.open("Map_sonar.png")
-# out_image = resizeimage.resize_width(img, 1200)
-# out_image.save("resize.png")
-#
-# img_map = ImageTk.PhotoImage(Image.open("resize.png"))
-#
-# label_map = tkinter.Label(frame_map, image=img_map)
-#
-# label_map.pack()
-
-robot = canva.create_oval(175, 100, 100, 175, width=3)
 
 
 def gui():
+    for i in range(500):
+        canva.move(robot, 0, 1)
+        window.update()
+        time.sleep(0.01)
     window.mainloop()
+
 
 
 def main():
     gui()
+
 
 
 main()
