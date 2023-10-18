c4b85098fac54ee68a80b3846f07a4c70f6fd41f
Hugo Tempez <hugotempezpro@gmail.com>
2023-10-18 17:10:04 +0200

    Robot en poo, lignes sonar ok

--- main.py
+++ main.py
@@ -1,63 +1,51 @@
 import time
 import tkinter
-from tkinter import filedialog as filedialog
+from tkinter import filedialog
 from PIL import ImageTk, Image
 from resizeimage import resizeimage
+from robot import Robot
 
 
+"""Taille de la fenêtre"""
 width: int = 1200
 height: int = 600
 
-
+"""Création de la fenêtre"""
 window = tkinter.Tk()
 chosen_map = tkinter.StringVar()
-window.geometry("1200x600")
+window.geometry("{}x{}".format(width, height))
 window.resizable(False, False)
 
+"""Initialisation du canva et formatage de l'image"""
 canva = tkinter.Canvas(width=width, height=height, background="white")
-# img = Image.open("Map_sonar.png")
-# resizeimage.resize_width(img, width).save("resize.png")
+img = Image.open("Map_sonar.png")
+resizeimage.resize_width(img, width).save("resize.png")
 
-# img_map = ImageTk.PhotoImage(Image.open("resize.png"))
-# canva.pack()
-# canva.create_image(width/2, height/2, anchor="center", image=img_map)
-#
-# robot = canva.create_oval(30, 30, 60, 60, width=3)
-# image = Image.open("resize.png")
+"""Import de l'image formaté et création du canva"""
+img_map = ImageTk.PhotoImage(Image.open("resize.png"))
+canva.pack()
+canva.create_image(width/2, height/2, anchor="center", image=img_map)
 
+"""Import de l'image en mémoire pour checker les limites"""
+image = Image.open("resize.png")
 
-# def gui():
-#     x = int(canva.coords(robot)[0] + canva.coords(robot)[2]) // 2
-#     y = int(canva.coords(robot)[1] + canva.coords(robot)[3]) // 2
-#     direction = "droite"
-#     while True:
-#         x, y = move(direction, x, y)
-#         rgb = image.getpixel((x, y))
-#         print("x = ", x, " ,y = ", y, " ,r = ", rgb[0], " ,g = ", rgb[1], ", b = ", rgb[2])
-#         if rgb[0] != 255 and rgb[1] != 255 and rgb[2] != 255:
-#             if direction == "droite":
-#                 direction = "gauche"
-#             elif direction == "gauche":
-#                 direction = "droite"
-#         window.update()
-#         time.sleep(0.00001)
-#     window.mainloop()
-#
-#
-# def move(direction, x, y):
-#     if direction == "droite":
-#         canva.move(robot, 1, 0)
-#         x += 1
-#     elif direction == "gauche":
-#         canva.move(robot, -1, 0)
-#         x -= 1
-#     elif direction == "haut":
-#         canva.move(robot, 0, -1)
-#         y -= 1
-#     elif direction == "bas":
-#         canva.move(robot, 0, 1)
-#         y += 1
-#     return x, y
+"""Initialisation du robot"""
+robot = Robot(canva, 30, 30, 30)
+
+def gui():
+    pos = robot.get_robot_position()
+    direction = "droite"
+    while True:
+        x, y = robot.move_robot(direction)
+        rgb = image.getpixel((x+15, y+15))
+        #print("x = ", x, " ,y = ", y, " ,r = ", rgb[0], " ,g = ", rgb[1], ", b = ", rgb[2])
+        if rgb[0] != 255 and rgb[1] != 255 and rgb[2] != 255:
+            if direction == "droite":
+                direction = "gauche"
+            elif direction == "gauche":
+                direction = "droite"
+        window.update()
+        time.sleep(0.00001)
 
 
 def user_inputs():
@@ -79,8 +67,9 @@ def dialogbox_choose_map():
 
 
 def main():
-    #gui()
-    user_inputs()
+    gui()
+    # user_inputs()
     window.mainloop()
+
 
 main()
---
+++ robot.py
@@ -0,0 +1,93 @@
+import tkinter
+import math
+
+
+class Robot:
+    def __init__(self, canva, x=30, y=30, diameter=30, rayon=0, nb_rayon=0):
+        """Diamétre et position du robot"""
+        self.diameter = diameter
+        self.rayon = diameter/2
+        self.x = x + self.rayon
+        self.y = y + self.rayon
+        """Position de la pointe extérieur de la ligne rouge"""
+        self.redline_x = self.x + self.rayon
+        self.redline_y = self.y
+        """Orientation en degrés"""
+        self.direction = 0
+        """Rayon de détection du sonar et nombre de capteur(s)"""
+        self.rayon_sonar = rayon
+        self.nb_rayon = nb_rayon
+        self.distance_rayon = 0
+        """Import du canva depuis le main programme"""
+        self.canva = canva
+        """Création du robot et de la ligne directrice, les details des paramètres sont les suivants :
+         param1 = debut x, param2 = debut y, param3 = fin x, param4 = fin y 
+         (ou inversement si param3 est plus petit que param1 par exemple)"""
+        self.robot = canva.create_oval(self.x-self.rayon, self.y-self.rayon,
+                                       self.x+self.rayon, self.y+self.rayon, width=2)
+        self.robot_direction = canva.create_line(self.x, self.y,
+                                                 self.redline_x, self.redline_y, fill="red", width=1)
+        self.has_sonar = False
+        self.lines = []
+        self.create_sonar()
+
+    def create_sonar(self, nb_rayon=10, rayon=180, distance=100):
+        occurences = int(rayon / (nb_rayon-1))
+        for deg in range(0, rayon+1, occurences):
+            rad = deg*math.pi/180
+            x = math.cos(rad)*distance
+            y = math.sin(rad)*distance
+            self.lines.append(self.canva.create_line(self.x, self.y, self.x+x, self.y+y, fill="blue", width=1))
+        self.has_sonar = True
+
+    def kill_sonar(self):
+        for element in self.lines:
+            tkinter.Canvas.delete(self.canva, element)
+        self.has_sonar = False
+
+    def move_robot(self, direction):
+        if direction == "droite":
+            self.canva.move(self.robot, 1, 0)
+            self.canva.move(self.robot_direction, 1, 0)
+            if self.has_sonar:
+                for i in range(len(self.lines)):
+                    self.canva.move(self.lines[i], 1, 0)
+            self.x += 1
+        elif direction == "gauche":
+            self.canva.move(self.robot, -1, 0)
+            self.canva.move(self.robot_direction, -1, 0)
+            if self.has_sonar:
+                for i in range(len(self.lines)):
+                    self.canva.move(self.lines[i], -1, 0)
+            self.x -= 1
+        elif direction == "haut":
+            self.canva.move(self.robot, 0, -1)
+            self.canva.move(self.robot_direction, 0, -1)
+            self.y -= 1
+        elif direction == "bas":
+            self.canva.move(self.robot, 0, 1)
+            self.canva.move(self.robot_direction, 0, 1)
+            self.y += 1
+        return self.x, self.y
+
+    def move_sonar(self):
+        return 0
+
+    def get_x(self):
+        return self.x
+
+    def set_x(self, x):
+        self.x = x
+
+    def get_y(self):
+        return self.y
+
+    def set_y(self, y):
+        self.y = y
+
+    def get_robot_position(self):
+        return {"x": [int(self.canva.coords(self.robot)[0]), int(self.canva.coords(self.robot)[2])],
+                "y": [int(self.canva.coords(self.robot)[1]), int(self.canva.coords(self.robot)[3])]}
+
+    def telemesure(self):
+        return 0
