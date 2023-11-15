0e5cfc1bedaa848228183449c0f9df4b97d7e693
Hugo Tempez <hugotempezpro@gmail.com>
2023-11-15 13:39:42 +0100

    HT - tmp

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import time
 import tkinter
 from tkinter import filedialog
 from PIL import ImageTk, Image
-from resizeimage import resizeimage
 from robot import Robot
-
 
 # Taille de la fenêtre
 WIDTH: int = 1200
@@ -17,58 +15,45 @@ window.geometry("{}x{}".format(WIDTH, HEIGHT))
 window.resizable(False, False)
 
 # Variables globales
+img_path = ""               # Path de l'image
 image: Image                # Image en mémoire pour le check des limites
 robot: Robot                # Instance de l'objet robot
+menu_frame: tkinter.Frame   # Frame pour le menu
 canva: tkinter.Canvas       # Canvas Tkinter
 
 
 def resize_image(file_path):
-    global image
-    img = Image.open(file_path)
-    img.resize((WIDTH, HEIGHT)).save("resize.png")
+    """Redimensionne une image si elle est trouvée, sinon lève une exception"""
+    if file_path:
+        img = Image.open(file_path)
+        img.resize((WIDTH, HEIGHT)).save("resize.png")
+        img.close()
+    else:
+        raise FileNotFoundError
 
 
-def init_sim():
-    global canva
-    """Initialisation du canva et formatage de l'image"""
-    canva = tkinter.Canvas(width=WIDTH, height=HEIGHT, background="white")
-    img_map = ImageTk.PhotoImage(Image.open("resize.png"))
-    canva.pack()
-    canva.create_image(WIDTH / 2, HEIGHT / 2, anchor="center", image=img_map)
-
-
-def user_inputs():
+def main_menu():
+    global menu_frame
+    menu_frame = tkinter.Frame(window)
+    menu_frame.pack()
     text_label = ("Choisissez votre map en cliquant sur le bouton ci-dessous."
                   "Pour information, les formats acceptés sont png, jpg et jpeg et"
                   " l'image sera automatiquement redimensionné en {}x{}".format(str(WIDTH), str(HEIGHT)))
-    label_map_choice = tkinter.Label(window, text=text_label)
+    label_map_choice = tkinter.Label(menu_frame, text=text_label)
     label_map_choice.pack()
-    button_map_choice = tkinter.Button(window, text="Choix de la map", command=dialogbox_choose_map)
+    button_map_choice = tkinter.Button(menu_frame, text="Choix de la map", command=dialogbox_choose_map)
     button_map_choice.pack()
-    label_map_chosen = tkinter.Label(window, textvariable=chosen_map)
+    label_map_chosen = tkinter.Label(menu_frame, textvariable=chosen_map)
     label_map_chosen.pack()
-    button_launch = tkinter.Button(window, text="Lancer la simulation", command=init_sim)
+    button_launch = tkinter.Button(menu_frame, text="Lancer la simulation", command=init_sim)
     button_launch.pack()
 
 
-def open_image():
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.png *.jpeg *.gif *.bmp")])
-    if file_path:
-        image = Image.open(file_path)
-        resized_image = image.resize((1200, 600))
-        photo = ImageTk.PhotoImage(resized_image)
-        canva = tkinter.Canvas(width=WIDTH, height=HEIGHT, background="white")
-        canva.pack()
-        canva.create_image(WIDTH / 2, HEIGHT / 2, anchor="center", image=photo)
-        label.config(image=photo)
-        label.photo = photo
-    else:
-        raise Exception
-
-
 def dialogbox_choose_map():
+    global img_path
     filetypes = [("Image files", "*.jpg *.png *.jpeg *.gif *.bmp")]
-    chosen_map.set(filedialog.askopenfilename(initialdir="/", title="Choisissez une image", filetypes=filetypes))
+    img_path = filedialog.askopenfilename(initialdir="./", title="Choisissez une image", filetypes=filetypes)
+    chosen_map.set(img_path)
 
 
 def on_mouse_click(eventorigin):
@@ -91,21 +76,48 @@ def key_bindings():
     canva.bind("<MouseWheel>", on_mouse_wheel)
 
 
-# Créer une étiquette pour afficher l'image
-label = tkinter.Label(window)
-label.pack()
-
-
 def popup():
+    """A modifier en messagebox"""
     top = tkinter.Toplevel(window)
     top.geometry("600x50")
     top.title("Instructions")
     tkinter.Label(top, text="Placez le robot en cliquant à un endroit de la map. "
-                            "Le robot est dirigeable avec la molette de la souris").pack()
+                            "Le robot est orientable avec la molette de la souris").pack()
+
+
+def return_to_menu():
+    global canva, menu_frame
+    canva.destroy()
+    menu_frame.pack()
+
+
+def init_sim():
+    """Initialisation du canva et chargement de l'image"""
+    global menu_frame, canva, image, img_path
+    if img_path != "":
+        try:
+            resize_image(img_path)
+            menu_frame.pack_forget()
+            canva = tkinter.Canvas(width=WIDTH, height=HEIGHT, background="red")
+            print(img_path)
+            image = Image.open("resize.png")
+            img_map = ImageTk.PhotoImage(image)
+            #canva.grid(row=1, column=1)
+            canva.pack()
+            canva.create_image(WIDTH/2, HEIGHT/2, anchor="center", image=img_map)
+            # bottom_frame = tkinter.Frame(canva)
+            # bottom_frame.grid(row=1, column=2)
+            # return_button = tkinter.Button(bottom_frame, text="Quitter", command=return_to_menu)
+            # return_button.pack()
+            key_bindings()
+            popup()
+        except Exception as e:
+            print(e)
+    else:
+        raise FileNotFoundError
 
 
 def gui():
-    popup()
     #pos = robot.get_robot_position()
     direction = "droite"
     while True:
@@ -122,9 +134,8 @@ def gui():
 
 
 def main():
-    popup()
+    main_menu()
     # gui()
-    user_inputs()
     window.mainloop()
 
 
